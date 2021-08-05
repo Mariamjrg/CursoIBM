6b973a1c8f889602e9861f01ff827438ec755eff
unknown <mjrg19@gmail.com>
2021-08-05 12:33:30 +0200

    Quitado un punto de comentario

--- Ejercicio008.py
+++ Ejercicio008.py
@@ -3,7 +3,7 @@
 # dos palabras, las cuales se guardarán en dos variables distintas. 
 # A continuación, almacená en una variable la concatenación de la 
 # primera palabra, más un espacio, más la segunda palabra. 
-# Mostrá este resultado en pantalla.
+# Mostrá este resultado en pantalla
 
 palabra1=input('palabra1:')
 palabra2=input('palabra2:')
